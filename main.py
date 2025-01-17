3d75cf4b72791bfb9300e4279918553fe57e63d5
fogel.bence <fogel.bence@hallgato.itk.ppke.hugit config --global --listlsgit remote addgit config --global user.email fogel.bence@hallgato.itk.ppke.hu>
2025-01-17 01:08:01 +0100

    Add comments

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pandas as pd
 import altair_saver
 from utils import *
 
+# Define the directories containing the input data
 input_dir = 'L:/cluster_seed30'
 t = np.load(input_dir + '/raw_data/taxis.npy')[:20000]
 v = np.load(input_dir + '/raw_data/membrane_potential_data/v.npy')[1471, :20000]  # 1471 soma(0.5)
+part_neg = pd.read_csv(input_dir + '/partitioned_data/total_currents/itotal_neg_0.csv')
+part_pos = pd.read_csv(input_dir + '/partitioned_data/total_currents/itotal_pos_0.csv')
 
-part_neg = pd.read_csv(input_dir + '/partitioned_data/soma_currents/soma_neg_0.csv')
-part_pos = pd.read_csv(input_dir + '/partitioned_data/soma_currents/soma_pos_0.csv')
+# Rename current type column
 part_neg.rename(columns={part_neg.columns[0]: 'itype'}, inplace=True)
 part_pos.rename(columns={part_pos.columns[0]: 'itype'}, inplace=True)
 
@@ -26,12 +28,13 @@ part_neg_without_itype = part_neg.iloc[:, 1:]
 part_neg_unique = part_neg_without_itype.iloc[:, unique_indices]
 part_neg_unique.insert(0, 'itype', part_neg.iloc[:, 0])
 
+# Create charts
 totalpos = create_currsum_pos_chart(part_pos_unique, t_unique)
 totalneg = create_currsum_neg_chart(part_neg_unique, t_unique)
 currshares_pos, currshares_neg = create_currshares_chart(part_pos_unique, part_neg_unique, t_unique)
 vm_chart = create_vm_chart(v_unique, t_unique)
 
-# Generate currentscape
+# Create currentscape
 currentscape = combine_charts(vm_chart, totalpos, currshares_pos, currshares_neg, totalneg)
 currentscape.save('currentscape.html')
 
